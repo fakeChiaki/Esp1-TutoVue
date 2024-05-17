177fe62aa8d48ae91415596ad0698396b2ff92c4
『Chiaki』 <windofdisaster@gmail.com>
2024-05-17 16:00:36 -0400

    Second Tutorial

---
+++ src/components/ComponenteHijo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card mt-3">
+        <div class="card-body">
+            <h5 class="card-title">Componente Hijo</h5>
+            <p class="card-text">{{ mensaje }}</p>
+            <button class="btn btn-success" v-on:click="emitirEvento">Cambiar Mensaje</button>
+            <p v-show="mensajeCambiado">El mensaje ha sido cambiado</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Hijo",
+    props: ["mensaje"],
+    data() {
+        return {
+            mensajeCambiado: false
+        }
+    },
+    methods: {
+        emitirEvento() {
+            this.mensajeCambiado = true;
+            this.emit$("actualizarMensaje", "Nuevo mensaje desde el Hijo");
+        }
+    }
+}
+</script>
--- tutorial-vue/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
-import Contador from "../components/Contador.vue";
-const routes = [
-    { path: '/', component: Contador },
-]
-export const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
-})
